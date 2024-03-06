4a3cc94a04b0220c5510f8d5701f499978219348
ujjwalchauhan960 <161251952+ujjwalchauhan960@users.noreply.github.com>
2024-03-06 22:26:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    var expression = document.getElementById('display').value;
+    var result = eval(expression);
+    document.getElementById('display').value = result;
+}
+
+function factorial() {
+    var num = parseInt(document.getElementById('display').value);
+    var result = 1;
+    for (var i = 1; i <= num; i++) {
+        result *= i;
+    }
+    document.getElementById('display').value = result;
+}
+
+function backspace() {
+    var displayValue = document.getElementById('display').value;
+    document.getElementById('display').value = displayValue.slice(0, -1);
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
